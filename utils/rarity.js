71e6c2caccbd5d87790a20e7c5c4268ea588f85a
Ozgur Atmaca <ozguratmaca@gmail.com>
2022-01-12 19:31:36 +0300

    Outputs rarity to separate file

--- utils/rarity.js
+++ utils/rarity.js
@@ -1,6 +1,7 @@
 const basePath = process.cwd();
 const fs = require("fs");
 const layersDir = `${basePath}/layers`;
+const buildDir = `${basePath}/build/json`;
 
 const { layerConfigurations } = require(`${basePath}/src/config.js`);
 
@@ -12,6 +13,7 @@ let data = JSON.parse(rawdata);
 let editionSize = data.length;
 
 let rarityData = [];
+let rarityDataOutput = {};
 
 // intialize layers to chart
 layerConfigurations.forEach((config) => {
@@ -21,23 +23,28 @@ layerConfigurations.forEach((config) => {
     // get elements for each layer
     let elementsForLayer = [];
     let elements = getElements(`${layersDir}/${layer.name}/`);
+
     elements.forEach((element) => {
       // just get name and weight for each element
       let rarityDataElement = {
-        trait: element.name,
+        trait: element.name.replace(/_/g, " "),
         weight: element.weight.toFixed(0),
         occurrence: 0, // initialize at 0
       };
       elementsForLayer.push(rarityDataElement);
     });
+
     let layerName =
       layer.options?.["displayName"] != undefined
         ? layer.options?.["displayName"]
         : layer.name;
-    // don't include duplicate layers
+
+      // don't include duplicate layers
     if (!rarityData.includes(layer.name)) {
+      // console.log('elementsForLayer', elementsForLayer);
       // add elements for each layer to chart
       rarityData[layerName] = elementsForLayer;
+      rarityDataOutput[layerName] = elementsForLayer;
     }
   });
 });
@@ -72,9 +79,14 @@ for (var layer in rarityData) {
   }
 }
 
+
+fs.writeFileSync(
+  `${buildDir}/rarityData.json`,
+  JSON.stringify(rarityDataOutput, null, 2)
+);
+
 // print out rarity data
 for (var layer in rarityData) {
-  console.log(`Trait type: ${layer}`);
   for (var trait in rarityData[layer]) {
     console.log(rarityData[layer][trait]);
   }
